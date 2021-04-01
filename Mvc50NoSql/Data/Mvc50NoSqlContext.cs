7d406cceaba464f0255c5b2309411d1b74b6ac77
Andy Wang <v-wajian@microsoft.com>
2021-03-31 20:00:22 -0700

    Add project files.

---
+++ Mvc50NoSql/Data/Mvc50NoSqlContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mvc50NoSql.Models;
+
+namespace Mvc50NoSql.Data
+{
+    public class Mvc50NoSqlContext : DbContext
+    {
+        public Mvc50NoSqlContext (DbContextOptions<Mvc50NoSqlContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mvc50NoSql.Models.Movie> Movie { get; set; }
+    }
+}
